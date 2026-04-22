23db94d6f334373139728114e36a2d52d5248a0e
Pratik Pattanaik <pratikpattanaik1992@outlook.com>
2026-04-22 08:45:29 +0530

    update security group

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ type        = "ingress"
 from_port   = 80
 to_port     = 80
 protocol    = "tcp"
-cidr_blocks = ["0.0.0.0/0]
+cidr_blocks = ["0.0.0.0/0"]
 
 security_group_id = aws_security_group.blog.id
 }
@@ -55,7 +55,7 @@ type        = "ingress"
 from_port   = 443
 to_port     = 443
 protocol    = "tcp"
-cidr_blocks = ["0.0.0.0/0]
+cidr_blocks = ["0.0.0.0/0"]
 
 security_group_id = aws_security_group.blog.id
 }
@@ -65,7 +65,7 @@ type        = "egress"
 from_port   = 0
 to_port     = 0
 protocol    = -1
-cidr_blocks = ["0.0.0.0/0]
+cidr_blocks = ["0.0.0.0/0"]"
 
 security_group_id = aws_security_group.blog.id
 }
