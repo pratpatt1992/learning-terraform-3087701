e3e748cd969be23d342621ef642a2a7a72decade
Pratik Pattanaik <pratikpattanaik1992@outlook.com>
2026-04-25 08:04:36 +0530

    remove ALB

--- main.tf
+++ main.tf
@@ -57,41 +57,41 @@ egress_rules = ["all-all"]
 egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
-module "blog_alb" {
-  source = "terraform-aws-modules/alb/aws"
+# module "blog_alb" {
+#   source = "terraform-aws-modules/alb/aws"
 
-  name    = "blog-alb"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
+#   name    = "blog-alb"
+#   vpc_id  = module.blog_vpc.vpc_id
+#   subnets = module.blog_vpc.public_subnets
 
-  security_groups = [module.blog_sg.security_group_id]
+#   security_groups = [module.blog_sg.security_group_id]
 
-  listeners = {
-    blog-http = {
-      port     = 80
-      protocol = "HTTP"
-      forward = {
-        target_group_arn = aws_lb_target_group.blog.arn
-      }
-    }
-  }
-  tags = {
-    Environment = "Dev"
-  }
-}
+#   listeners = {
+#     blog-http = {
+#       port     = 80
+#       protocol = "HTTP"
+#       forward = {
+#         target_group_arn = aws_lb_target_group.blog.arn
+#       }
+#     }
+#   }
+#   tags = {
+#     Environment = "Dev"
+#   }
+# }
 
-resource "aws_lb_target_group" "blog" {
-  name     = "blog-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = module.blog_vpc.vpc_id
-}
+# resource "aws_lb_target_group" "blog" {
+#   name     = "blog-tg"
+#   port     = 80
+#   protocol = "HTTP"
+#   vpc_id   = module.blog_vpc.vpc_id
+# }
 
-resource "aws_lb_target_group_attachment" "blog" {
-  target_group_arn = aws_lb_target_group.blog.arn
-  target_id        = aws_instance.web.id
-  port             = 80
-}
+# resource "aws_lb_target_group_attachment" "blog" {
+#   target_group_arn = aws_lb_target_group.blog.arn
+#   target_id        = aws_instance.web.id
+#   port             = 80
+# }
 
 # resource "aws_security_group" "blog" {
 #   name        = "blog"
